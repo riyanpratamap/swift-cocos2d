aa1de6c0aeaa69104866c9ac2884a6ccce95f227
Akip Maulana <akip.maulana@gmail.com>
2019-10-06 14:33:41 +0700

    Initial Commit

---
+++ Avatar-Experiment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Avatar-Experiment
+//
+//  Created by Akip Maulana on 06/10/19.
+//  Copyright © 2019 Akirah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Avatar-ExperimentTests/Avatar_ExperimentTests.swift
@@ -0,0 +1,34 @@
+//
+//  Avatar_ExperimentTests.swift
+//  Avatar-ExperimentTests
+//
+//  Created by Akip Maulana on 06/10/19.
+//  Copyright © 2019 Akirah. All rights reserved.
+//
+
+import XCTest
+@testable import Avatar_Experiment
+
+class Avatar_ExperimentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
